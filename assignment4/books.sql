34140bd76ae8044f76991b37ec557cef757161f7
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2019-03-25 22:44:13 -0500

    Done assignment 4.

--- assignment4/books.sql
+++ assignment4/books.sql
@@ -10,3 +10,21 @@ ALTER TABLE `Authors` ADD INDEX(`Author_id`);
 ALTER TABLE `Book_Authors` ADD FOREIGN KEY (`Book_id`) REFERENCES `Book`(`Book_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 ALTER TABLE `Book_Authors` ADD FOREIGN KEY (`Author_id`) REFERENCES `Authors`(`Author_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+INSERT INTO `Book` (`Book_id`, `Title`, `Year`, `Price`, `Category`) VALUES ('1', 'Everyday Italian', '2005', '30.00', 'cooking');
+INSERT INTO `Book` (`Book_id`, `Title`, `Year`, `Price`, `Category`) VALUES ('2', 'Harry Potter', '2005', '30.00', 'children');
+INSERT INTO `Book` (`Book_id`, `Title`, `Year`, `Price`, `Category`) VALUES ('3', 'XQuery Kick Start', '2003', '49.99', 'web');
+INSERT INTO `Book` (`Book_id`, `Title`, `Year`, `Price`, `Category`) VALUES ('4', 'Learning XML', '2003', '39.95', 'web');
+
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('1', 'Giada De Laurentiis');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('2', 'J K. Rowling');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('3', 'James McGovern');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('4', 'Per Bothner');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('5', 'Kurt Cagle');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('6', 'James Linn');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('7', 'Vaidyanathan Nagarajan');
+INSERT INTO `Authors` (`Author_id`, `Author_Name`) VALUES ('8', 'Erik T. Ray');
+
+INSERT INTO `Book_Authors` (`Book_id`, `Author_id`) VALUES ('1', '1'), ('2', '2');
+INSERT INTO `Book_Authors` (`Book_id`, `Author_id`) VALUES ('3', '3'), ('3', '4');
+INSERT INTO `Book_Authors` (`Book_id`, `Author_id`) VALUES ('3', '5'), ('3', '6');
+INSERT INTO `Book_Authors` (`Book_id`, `Author_id`) VALUES ('3', '7'), ('4', '8');
--- assignment4/index.php
+++ assignment4/index.php
@@ -1,75 +1,82 @@
 <?php
-  session_start();
-  require_once("BookRouteHandler.php");
+session_start();
+require_once("BookRouteHandler.php");
 
-  $view = "";
-  if(isset($_GET["view"]))
-	  $view = $_GET["view"];
+$view = "";
+if (isset($_GET["view"])) $view = $_GET["view"];
 
-  $request_uri = explode('?', $_SERVER['REQUEST_URI'], null);
+$request_uri = explode('/', $_SERVER['REQUEST_URI']);
+//var_dump($request_uri);
 
   //connect to database
-  $con = mysqli_connect("localhost","root","root","PW7");
-  /* check connection */
-  if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-  }
+$con = mysqli_connect("localhost", "root", "root", "HW4");
+/* check connection */
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
 
-  // Route it up!
-  switch($request_uri[0]){
-    case '/':
-      get_root($con);
-      break;
-    case '/books':
-        get_book($con, $request_uri);
-        break;
-      default:
-        echo "Invalid route.";
-  }
+// Route it up!
+switch ($request_uri[1]) {
+	case '':
+		get_root($con);
+		break;
 
-  function get_root($con){
-    $sql = "SELECT * FROM Book";
+	case 'books':
+		get_book($con, $request_uri);
+		break;
+
+	default:
+		echo "Invalid route.";
+}
+
+function get_root($con)
+{
+	$sql = "SELECT * FROM Book";
     //echo $sql."<br>";
 
-    $result=mysqli_query($con,$sql);
-    //var_dump($result);
-    $rows = array();
+	$result = mysqli_query($con, $sql);
+	$rows = array();
 
-    if($result == FALSE){
-      echo "SQL error, turn this on to debug: mysqli_report(MYSQLI_REPORT_ALL);";
-    }else {
-      foreach($result as $row){
-        $rows[]= $row;
-      }
-      print json_encode($rows);
-    }
+	if ($result == FALSE) {
+		echo "SQL error, turn this on to debug: mysqli_report(MYSQLI_REPORT_ALL);";
+	} else {
+		foreach ($result as $row) {
+			$rows[] = $row;
+		}
+		print json_encode($rows);
+	}
+}
 
-    mysqli_close($con);
-  }
+function get_book($con, $request_uri)
+{
+	if ($request_uri[2]) {
+		$sql = "SELECT Book.Book_id, Title, Year, Price, Category, GROUP_CONCAT(Authors.Author_Name) AS Author FROM Book 
+    INNER JOIN Book_Authors ON Book.Book_id = Book_Authors.Book_id 
+    INNER JOIN Authors ON Book_Authors.Author_id = Authors.Author_id 
+    WHERE Book.Book_id = $request_uri[2]
+    GROUP BY Book.Book_id";
+	} else {
+		$sql = "SELECT Book.Book_id, Title, Year, Price, Category, GROUP_CONCAT(Authors.Author_Name) AS Author FROM Book 
+    INNER JOIN Book_Authors ON Book.Book_id = Book_Authors.Book_id 
+    INNER JOIN Authors ON Book_Authors.Author_id = Authors.Author_id
+    GROUP BY Book.Book_id";
+	}
 
-  function get_book($con, $request_uri){
-    if(isset($request_uri[1]) && $request_uri[1] == ''){
-      get_root($con);
-    } elseif($request_uri[1]){
-      $sql = "SELECT * FROM Book WHERE Book_id = $request_uri[1]"; //need to join
-      $result=mysqli_query($con,$sql);
-      //var_dump($result);
-      $rows = array();
+	$result = mysqli_query($con, $sql);
+	$rows = array();
 
-
-      if($result == FALSE){
-        echo "SQL error, turn this on to debug: mysqli_report(MYSQLI_REPORT_ALL);";
-      }else {
-        foreach($result as $row){
-          $rows[]= $row;
-        }
-        print json_encode($rows);
-      }
-
-    }
-
-  }
-
+	if ($result == FALSE) {
+		echo "SQL error, turn this on to debug: mysqli_report(MYSQLI_REPORT_ALL);";
+	} else if(mysqli_num_rows($result) == 0){
+    echo "No result";
+  } else {
+		foreach ($result as $row) {
+			$rows[] = $row;
+		}
+		print json_encode($rows);
+	}
+}
+mysqli_close($con);
 
 ?>
